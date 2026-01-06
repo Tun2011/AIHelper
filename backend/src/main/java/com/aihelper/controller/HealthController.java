3c20be81a32b9b4a1d949250c7f9c572e5ae9037
Tuntoine <nguyentuananh20112004@gmail.com>
2026-01-06 14:02:15 +0700

    Fix HealthController: Add @CrossOrigin and explicit response

--- backend/src/main/java/com/aihelper/controller/HealthController.java
+++ backend/src/main/java/com/aihelper/controller/HealthController.java
@@ -1,6 +1,7 @@
 package com.aihelper.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin; // <--- NHỚ IMPORT CÁI NÀY
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+@CrossOrigin(origins = "*") 
 @RestController
 public class HealthController {
 
